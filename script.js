0aa368ad1af3ea2b28a98710b58e0d724cf4ac2d
main_character <97624417+rxxddy@users.noreply.github.com>
2024-05-18 13:19:52 +0200

    4rf

--- script.js
+++ script.js
@@ -6,9 +6,9 @@ img.src = "https://i.ibb.co/Q9yv5Jk/flappy-bird-set.png";
 
 // General settings
 let gamePlaying = false;
-const gravity = 0.5; // Adjusted gravity for smoother motion
+const gravity = 1; // Adjusted gravity for smoother motion
 const size = [51, 36];
-const jump = -10;
+const jump = -9;
 const cTenth = (canvas.width / 10);
 
 let index = 0,
